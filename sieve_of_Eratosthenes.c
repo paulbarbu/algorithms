7a59b904352ece1d863941ed5999247ef2bd8d6f
Paul Barbu <paullik@yahoo.com>
2011-10-01 20:27:41 +0300

    Added Sieve of Eratosthenes

---
+++ sieve_of_Eratosthenes.c
@@ -0,0 +1,30 @@
+/**
+ * Using the Sieve of Eratosthenes display all prime numbers smaller(or equal) than "n"
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int n;
+
+    scanf("%d", &n);
+
+    int *sieve = malloc(sizeof(int) * n);
+
+    for(int i=2; i<=n; i++){
+        for(int j=i+i; j<=n; j=j+i){
+            sieve[j] = -1;
+        }
+    }
+
+    for(int i=2; i<=n; i++){
+        printf("%d ", sieve[i]);
+    }
+
+    printf("\n");
+
+    free(sieve);
+
+    return 0;
+}
